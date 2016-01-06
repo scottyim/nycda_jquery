fc3c4a3600a47ec8496a161d3a1311ec693ac0e7
Scott Yim <scottyim@gmail.com>
2016-01-05 20:35:19 -0500

    jquery cheatsheet structure

--- jquerycheatsheet.js
+++ jquerycheatsheet.js
@@ -3,12 +3,22 @@
 // jQuery Cheat Sheet
 
 /* CONTENTS
-1. Intro */
+1. Intro
+2. Examples */
 
-// 1. Intro
-// - 
+// 1. Intro - jQuery
+// - an example of a library (external file written to invoke methods and functions to make tasks within another file easier/more efficient)
+// - written in JavaScript
+// - helps navigate DOM much easier/faster
+// - use $("___")
+// - don't forget to wrap inside/include $document.ready()
 
+// 2. Examples
 
+// a. hide
 
+// b. show
 
+// c. onclick
 
+// d. fade
